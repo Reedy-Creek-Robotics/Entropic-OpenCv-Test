81b50027b6769fbe23f8edf86630dba81064a94b
Sean Stephenson <sean.stephenson@taulia.com>
2023-02-07 11:13:13 -0800

    Add HSV conversion

--- src/test/groovy/org/entropic/PoleDetectionTest.groovy
+++ src/test/groovy/org/entropic/PoleDetectionTest.groovy
@@ -23,23 +23,28 @@ class PoleDetectionTest {
 		def imageFile = new File(System.getProperty('user.dir'), '/src/test/resources/pole-at-4.25-in.bmp')
 		def image = Imgcodecs.imread(imageFile.absolutePath)
 
-		def color = new Scalar(0x00,0x50,0x7D)
-		def lowerBound = new Scalar(0x60,0x30,0x00)
-		def upperBound = new Scalar(0xFF,0x80,0x30)
+		// Convert from RGB to HSV so the detection is more tolerant of changes in lighting
+		Imgproc.cvtColor(image, image, Imgproc.COLOR_RGB2HSV)
 
+		// This is the measured color of the pole, in HSV space
+		def color = new Scalar(21, 201, 138)
+		def lowerBound = new Scalar(10,0,0)
+		def upperBound = new Scalar(30,255,255)
+
+		// Threshold the image so that we have a mask of only the pixels that are close to the pole color
 		def threshold = new Mat()
-
 		Core.inRange(image, lowerBound, upperBound, threshold)
 		Imgcodecs.imwrite('pole-detection-threshold.jpg', threshold)
 
+		// Use morphology "close" and "open" to remove small batches of noisy pixels and leave only larger regions.
 		def morphed = new Mat()
-		def temp = new Mat()
 		def kernel = Mat.ones(7, 7, CvType.CV_8U)
-		Imgproc.morphologyEx(threshold, temp, Imgproc.MORPH_CLOSE, kernel)
-		Imgproc.morphologyEx(temp, morphed, Imgproc.MORPH_OPEN, kernel)
+		Imgproc.morphologyEx(threshold, morphed, Imgproc.MORPH_CLOSE, kernel)
+		Imgproc.morphologyEx(morphed, morphed, Imgproc.MORPH_OPEN, kernel)
 
 		Imgcodecs.imwrite('pole-detection-morphed.jpg', morphed)
 
+		// Find the countours on the image, which are the connected regions of white pixels.
 		List<MatOfPoint> contours = []
 		Mat hierarchy = new Mat()
 		Imgproc.findContours(morphed, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE)
@@ -47,12 +52,14 @@ class PoleDetectionTest {
 		def contoured = new Mat()
 		image.copyTo(contoured)
 
+		// Draw the contours to the screen.
 		def red = new Scalar(0, 0, 255)
 		def green = new Scalar(0, 255, 0)
 		for (int i = 0; i < contours.size(); i++) {
 			Imgproc.drawContours(contoured, contours, i, red, 2, Imgproc.LINE_8)
 		}
 
+		// todo: Here you would want to sort the contours by their area and select only the largest one
 		println "Found ${contours.size()} contours"
 		assert !contours.empty
 		def contour = contours[0]
